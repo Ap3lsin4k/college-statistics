2bd653fe8bdd981fafbbb9a643e4947dc7c681bb
Andy Theodorko <andrii.fedorko@smashdocs.net>
2021-09-12 11:08:15 +0300

    add ability to evaluate each element of a series

--- test/test_series.py
+++ test/test_series.py
@@ -11,17 +11,22 @@ class Series:
 
         self.diverges = self.value == float('inf')
 
-
     def __eq__(self, other):
-
         return pytest.approx(self.value, 0.1) == other
-
 
     def __repr__(self):
         return str(float(self.value))
 
     def __str__(self):
         return " + ".join(map(str, [self.a(n) for n in range(1, 10)])) + " + ..."
+
+    def __iter__(self):
+        self.elem_index = 0
+        return self
+
+    def __next__(self):
+        self.elem_index += 1
+        return self.a(self.elem_index)
 
 def test_series():
     s = Series(lambda n: Fraction(1, 2**n))
@@ -30,5 +35,13 @@ def test_series():
     assert s == 1
 
     assert not s.diverges
+
+def test_natural_num():
+    natural = Series(lambda n: n)
+    natural_num_sequence = iter(natural)
+    assert next(natural_num_sequence) == 1
+    assert next(natural_num_sequence) == 2
+    assert next(natural_num_sequence) == 3
+    assert next(natural_num_sequence) == 4
 
 
