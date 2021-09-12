8f30587c2b9b0034eb62aa13a67f86c3ec155988
Andy Theodorko <andrii.fedorko@smashdocs.net>
2021-09-12 11:04:51 +0300

    birthday paradox: accurate naming

--- test/test_birthday_paradox.py
+++ test/test_birthday_paradox.py
@@ -2,10 +2,10 @@ import random
 from time import sleep
 
 import pytest
+from fractions import Fraction
 
 
-class Series:
-    pass
+
 
 
 def present_experiment(experiments, group_size, shared):
@@ -29,6 +29,7 @@ class Person(int):
 class Group:
     def __init__(self, persons):
         self.persons = persons
+        self.unique_birthdays = len(set(self.persons))
 
     @property
     def shared_birthday(self):
@@ -36,7 +37,7 @@ class Group:
 
     @property
     def how_many_people_share_birthday(self):
-        return len(self.persons) - len(set(self.persons)) + 1
+        return len(self.persons) - self.unique_birthdays
 
 
 def run_simulation(groups, people_in_a_group=30):
@@ -44,14 +45,17 @@ def run_simulation(groups, people_in_a_group=30):
     for i in range(groups):
         group = RandomGroup(people_in_a_group)
 
-        # print(f"group #{i}, {group.how_many_people_share_birthday} people \"share\" birthday")
+        print(f"group #{i}, {len(set(group.persons))} unique birthdays out of {people_in_a_group}")
 
         yield group.shared_birthday
 
 
 def test_random():
-    random_range = random.randint(0, 1)
-    assert 0 == random_range or 1 == random_range
+    random_range = random.randint(1, 4)
+    assert 4 == random_range \
+        or 1 == random_range \
+        or 2 == random_range \
+        or 3 == random_range
     number_of_people_in_a_group: int = 23
 
 
@@ -83,29 +87,21 @@ def test_group():
 
 class RandomGroup(Group):
     def __init__(self, num_of_people):
-        super().__init__([Person(birthday=random.randint(1, 365)) for _ in range(num_of_people)])
+        super().__init__([Person(birthday=random.randint(1, 4)) for _ in range(num_of_people)])
         self.num_of_people = num_of_people
 
 
 def test_birthday_paradox():
-    simulation = 0
-    runs = 64
-    accuracy = 64
-    for i in range(runs*accuracy):
-        simulation += bool(run_birthday_paradox())
-    print(f"\nChance of a shared group for six groups:")
-    print(f"\tExperimental - {simulation}/{accuracy*runs}, {(simulation*100.0)/accuracy/runs}%")
-    print(f"\tTheoretical - {63*accuracy}/{accuracy*runs}, {6300/runs}%")
-    diff = ((simulation*100.0)/accuracy/runs) - 6300/runs
-    print(f"{diff}%")
-
-def run_birthday_paradox():
-    experiments = 6
-    group_size = 23
+    experiments = 8000
+    group_size = 3
     theoretical_chance_of_shared_birthday = 0.706
     shared = 0
     for does_group_share_birthday in run_simulation(experiments, group_size):
         if does_group_share_birthday:
-            shared += 1  #     print(f"{does_group_share_birthday+1} people share birthday")  # else:  #     print(f"no people share birthday")
+            shared += 1  # print(f"{does_group_share_birthday+1} people share birthday")  # else:  #     print(f"no people share birthday")
     present_experiment(experiments, group_size, shared)
     return shared
+
+
+def test_fraction_multiplier():
+    assert Fraction(3/4)*Fraction(2/4) == Fraction(3/8)
