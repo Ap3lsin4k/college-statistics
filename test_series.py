16097324cb9f1c63712c033e82c676644c1cf819
Andy Theodorko <andy@pop-os.localdomain>
2021-10-19 09:07:55 +0300

    update test_series to add a problem from Khan Academy

--- test_series.py
+++ test_series.py
@@ -7,9 +7,9 @@ from fractions import Fraction
 class Series:
     infinity = 100
 
-    def __init__(self, a=lambda n: Fraction(), seq=None, infinity_power=5):
+    def __init__(self, a=lambda n: Fraction(), seq=None, infinity_power=3):
         self.a = a
-        self.value = sum([self.a(n) for n in range(1, 10**infinity_power)])
+        self.value = sum([self.a(n) for n in range(1, 10 ** infinity_power)])
 
         self.diverges = self.value == float('inf')
 
@@ -31,14 +31,14 @@ class Series:
         return self.a(self.elem_index)
 
 
-
 def test_series():
-    s = Series(lambda n: Fraction(1, 2**n))
+    s = Series(lambda n: Fraction(1, 2 ** n))
     assert s == s
 
     assert s == 1
 
     assert not s.diverges
+
 
 def test_natural_num():
     natural = Series(lambda n: n)
@@ -50,21 +50,22 @@ def test_natural_num():
 
 
 def test_element_of_series():
-    problem_1_5 = Series(lambda n: Fraction(n, 2**n * (n + 1)))
+    problem_1_5 = Series(lambda n: Fraction(n, 2 ** n * (n + 1)))
     iter(problem_1_5)
-    next(problem_1_5) # 1
+    next(problem_1_5)  # 1
     next(problem_1_5)
     next(problem_1_5)
     next(problem_1_5)
-    next(problem_1_5) # 5
+    next(problem_1_5)  # 5
     next(problem_1_5)
     next(problem_1_5)
-    next(problem_1_5) # 8
+    next(problem_1_5)  # 8
     next(problem_1_5)
     assert next(problem_1_5) == problem_1_5.a(10)
 
+
 def test_problem_1_5_2():
-    problem_1_5_2 = Series(lambda n: Fraction(3**n, math.factorial(n)))
+    problem_1_5_2 = Series(lambda n: Fraction(3 ** n, math.factorial(n)))
     problem_1_5_2 = iter(problem_1_5_2)
     assert next(problem_1_5_2) == 3
     assert next(problem_1_5_2) == 9 / 2
@@ -81,6 +82,7 @@ class LogicSeries:
     def __next__(self):
         return "1/7*5"
 
+
 @pytest.mark.skip("not implemented")
 def test_logical_guess():
     l = LogicSeries("1/1*3", "1/3*5", "1/5*7")
@@ -94,12 +96,16 @@ def test_logical_guess():
 class PartialSum:
     def __init__(self, finite_sum):
         def a(n):
-            if n < 1: raise ValueError("Indexing starts from 1")
-            if n == 1: return finite_sum(1)
-            else: return finite_sum(n) - finite_sum(n-1)
+            if n < 1:
+                raise ValueError("Indexing starts from 1")
+            if n == 1:
+                return finite_sum(1)
+            else:
+                return finite_sum(n) - finite_sum(n - 1)
+
         self.a = a
 
-        self.series = None# Series(finite_sum)
+        self.series = None  # Series(finite_sum)
     #
     # def a(self, param):
     #     pass
@@ -107,44 +113,68 @@ class PartialSum:
 
 def test_partial_sum():
     def partial_sum(num_of_elems):
-        n = num_of_elems # in a sum
+        n = num_of_elems  # in a sum
         return Fraction(n * (n + 3), 4 * (n + 1) * (n + 2))
 
     part_sum = PartialSum(partial_sum)
-    assert part_sum.a(1) == Fraction(1, 1*2*3)
-    assert part_sum.series != Series(lambda n: Fraction(n*(n+3), 4*(n+1)*(n+2)))
-    assert Fraction(5, 2 * 2*2 * 3) - Fraction(4, 24) == part_sum.a(2)
-    assert Fraction(1, 3 * (3 + 1) * (3+2)) == part_sum.a(3)
-    assert Fraction(1, 4 * 2*3 *5) == part_sum.a(4)
-    assert Fraction(1, (lambda n : n * (n + 1) * (n + 2))(5)) == part_sum.a(5)
-    assert Fraction(1, (lambda n : n * (n + 1) * (n + 2))(6)) == part_sum.a(6)
+    assert part_sum.a(1) == Fraction(1, 1 * 2 * 3)
+    assert part_sum.series != Series(lambda n: Fraction(n * (n + 3), 4 * (n + 1) * (n + 2)))
+    assert Fraction(5, 2 * 2 * 2 * 3) - Fraction(4, 24) == part_sum.a(2)
+    assert Fraction(1, 3 * (3 + 1) * (3 + 2)) == part_sum.a(3)
+    assert Fraction(1, 4 * 2 * 3 * 5) == part_sum.a(4)
+    assert Fraction(1, (lambda n: n * (n + 1) * (n + 2))(5)) == part_sum.a(5)
+    assert Fraction(1, (lambda n: n * (n + 1) * (n + 2))(6)) == part_sum.a(6)
 
     with pytest.raises(ValueError):
         part_sum.a(0)
 
     for n in range(6, 10):
-        assert part_sum.a(n) == partial_sum(n) - partial_sum(n-1)
-        assert part_sum.a(n) == Fraction(n * (n + 3),           4 * (n + 1) * (n + 2)) - Fraction((n-1) * ((n-1) + 3), 4 * ((n-1) + 1) * ((n-1) + 2))
-        assert part_sum.a(n) != Fraction(n * (n + 3) - (n-1) * ((n-1) + 3), 4 * (n + 1) * (n + 2) - 4 * ((n-1) + 1) * ((n-1) + 2))
+        assert part_sum.a(n) == partial_sum(n) - partial_sum(n - 1)
+        assert part_sum.a(n) == Fraction(n * (n + 3), 4 * (n + 1) * (n + 2)) - Fraction((n - 1) * ((n - 1) + 3),
+                                                                                        4 * ((n - 1) + 1) * (
+                                                                                                (n - 1) + 2))
+        assert part_sum.a(n) != Fraction(n * (n + 3) - (n - 1) * ((n - 1) + 3),
+                                         4 * (n + 1) * (n + 2) - 4 * ((n - 1) + 1) * ((n - 1) + 2))
     # assert partial_sum(3) ==
+
 
 class InfiniteSeq:
     def __init__(self, a):
         pass
 
-def test():
 
-    seq = InfiniteSeq(lambda n: Fraction(n*(n+3), 4*(n+1)*(n+2)))
+def test():
+    seq = InfiniteSeq(lambda n: Fraction(n * (n + 3), 4 * (n + 1) * (n + 2)))
     series = Series(seq=seq)
 
 
+# noinspection PyTypeChecker
+@pytest.mark.skip("Slow")
 def test_term_paper_1_2():
-    a = Series(lambda n: math.log(n)/math.sqrt(n**8+n), infinity_power=6)
-    assert Series(lambda n: math.log(n)/math.sqrt(n**8+n), infinity_power=1) == Series(lambda n: math.log(n)/math.sqrt(n**8+n), infinity_power=3)
-    print(3/43)
-    #assert  == 20.20
+    # a = Series(lambda n: math.log(n)/math.sqrt(n**8+n), infinity_power=6)
+    assert Series(lambda n: math.log(n) / math.sqrt(n ** 8 + n), infinity_power=1) == Series(
+        lambda n: math.log(n) / math.sqrt(n ** 8 + n), infinity_power=3)
+    print(3 / 43)
+    # assert  == 20.20
 
+
+# noinspection PyTypeChecker
+@pytest.mark.skip("Slow")
 def test_term_paper_1_3():
-    a = Series(lambda n: math.sin(math.sqrt(n)/math.sqrt(n**5+2)), infinity_power=8)
+    a = Series(lambda n: math.sin(math.sqrt(n) / math.sqrt(n ** 5 + 2)), infinity_power=8)
     print(a)
     assert a == 1.18
+
+
+def test_khan_academy_college_calculus_conv_div():
+    a = lambda n: Fraction(5 ** (2 * n), 4 ** (3 * n))
+    s = Series(a, infinity_power=1)
+    print(s)
+    print(repr(s))
+    assert s == Series(a, infinity_power=2)
+    print(repr(s), repr(Series(a, infinity_power=2)))
+
+
+def test_c():
+    n = 2
+    assert math.log(n)/n*n > Fraction(1, n*n)
